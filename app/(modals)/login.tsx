409fa7eaacb42edcac40590538ac45b111fa2d72
Hamid Ali <hamid.pak013@gmail.com>
2025-07-20 18:51:19 +0500

    complete login page design and added webview warmup hooks

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,101 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
-  return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
-}
+  useWarmUpBrowser();
 
-export default Page
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Email"
+        autoCapitalize="none"
+        style={[defaultStyles.inputfield, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorView}>
+        <View
+          style={{
+            flex: 1,
+            height: StyleSheet.hairlineWidth,
+            backgroundColor: "#ABABAB",
+            marginVertical: 20,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            height: StyleSheet.hairlineWidth,
+            backgroundColor: "#ABABAB",
+            marginVertical: 20,
+          }}
+        />
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="call-outline" size={24} color="#000" style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="logo-apple" size={24} color="#000" style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Sign in with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="logo-google" size={24} color="#000" style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Sign in with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="logo-facebook" size={24} color="#000" style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Sign in with Facebook</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  separatorView: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: "mon-sb",
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: "#FFFFFF",
+    height: 50,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000000',
+    fontSize: 16,
+    fontFamily: "mon-sb",
+  },
+});
+export default Page;
---
+++ hooks/useWarmUpBrowser.ts
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import * as WebBrowser from "expo-web-browser";
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
